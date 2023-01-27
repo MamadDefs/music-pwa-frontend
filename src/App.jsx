e52ce9f069c87c3e8e61d8a599f3f4b61c6c9fca
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2023-01-27 17:35:32 +0330

    p

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import Header from './componenets/Header/Header';
 import LoadingOval from './componenets/LoadingOval/LoadingOval';
-import sw from './sw'
+
 
 
 
