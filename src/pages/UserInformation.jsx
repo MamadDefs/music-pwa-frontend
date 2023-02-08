1eef38b91765f8caf3b33e7e708b2d52af4dfd33
Mohamadreza Habibzade <saoshyant1999@yahoo.com>
2023-02-08 23:17:34 +0330

    completing useinformation page

--- src/pages/UserInformation.jsx
+++ src/pages/UserInformation.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const UserInformation = () => {
+const UserInformation = ({ userInfo }) => {
     return (
-        <div>UserInformation</div>
+        <div className="page-content-holder">
+            <div className="account-information">
+                <br />
+                <h3>نام کاربری</h3>
+                <h4>{userInfo?.username}</h4>
+                <br />
+                <h3>ایمیل</h3>
+                <h4>{userInfo?.email}</h4>
+                <br />
+                <h3>نوع اکانت</h3>
+                <h4>{userInfo?.type? userInfo?.type : 'معمولی‌'}</h4>
+            </div>
+        </div>
     )
 }
 
